1072d026009cb6b275058e28cc3215dfaca3cbba
Martynov Evgeny <130037263+MartynovEvgeny@users.noreply.github.com>
2023-08-08 20:41:46 +0300

    Project created

---
+++ ClassWork_11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassWork_11
+//
+//  Created by Мартынов Евгений on 8.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
